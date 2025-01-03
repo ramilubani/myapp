116e600bb1a7a1ae832827f1a5d6017604d4b57e
ramilubani <ramilubani@yahoo.com>
2025-01-04 00:39:22 +0100

    2708

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,19 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
     public function register(Request $request){
         $incomingFields = $request->validate([
-            'name' => ['required', 'min:3', 'max:255'],
-            'email' => ['required', 'email'],
+            'name' => ['required', 'min:3', 'max:255', Rule::unique ('users', 'name')],
+            'email' => ['required', 'email', 'max:255', Rule::unique ('users', 'email')],
             'password' => ['required', 'min:6', 'max:255']
         ]);
 
         $incomingFields['password'] = bcrypt($incomingFields['password']);
 
-        User::create($incomingFields);
-        return "Hello from register controller";
+        $user = User::create($incomingFields);
+        auth()->login($user);
+
+        return redirect('/');
     }
 }
