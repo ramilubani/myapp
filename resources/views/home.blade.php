d78bbc943204d3d65824b23830aab7b678f9104b
ramilubani <ramilubani@yahoo.com>
2025-01-03 18:14:03 +0100

    1125

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,8 @@
 <body>
    <div style="border: 3px solid black;">
     <h2>Register</h2>
-    <form action="">
+    <form action="/register" method="post">
+    @csrf
         <input type="text" name="" placeholder="name">
         <input type="text" name="" placeholder="email">
         <input type="password" name="" placeholder="password">
