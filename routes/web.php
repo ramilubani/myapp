b3bd2fc97bef3c423f7d0053ae6f976b7f8db413
ramilubani <ramilubani@yahoo.com>
2025-01-07 12:24:26 +0100

    blog post controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view ("home");
@@ -12,4 +13,6 @@ Route::get('/', function () {
 Route::post('/register', [UserController::class, 'register'] );
 Route::post('/logout', [UserController::class, 'logout'] );
 Route::post('/login', [UserController::class, 'login'] );
-Route::post('/login', [PostController::class, 'login'] );
+
+//blog posts
+Route::post('/create-post', [PostController::class, 'createPost'] );
